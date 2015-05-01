c3856ccc5f2f92fed65ec3fbd788ca73f47f3314
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2015-05-01 11:32:00 +0200

    Added settle functionality to debt

--- www/app/debts/debts.controller.js
+++ www/app/debts/debts.controller.js
@@ -6,25 +6,67 @@
 
     DebtsController.inject = ['$scope', 'persistenceService'];
     function DebtsController($scope, persistenceService) {
-        var vm = this;
+        var vm = this,
+            data;
 
         /// Data
         vm.debts = [];
 
+        /// Actions
+        vm.refresh = refresh;
+        vm.toggleDebt = toggleDebt;
+
         /// Events
-        $scope.$on('$ionicView.enter', initialize);
+        $scope.$on('$ionicView.beforeEnter', initialize);
 
         /// Implementation
         function initialize() {
-            vm.debts = persistenceService
-                .readAll()
+            loadData();
+        }
+
+        function refresh() {
+            loadData();
+            $scope.$broadcast('scroll.refreshComplete');
+        }
+
+        function loadData() {
+            data = persistenceService.readAll();
+            vm.debts = data
+                .filter(function(x) {
+                    return x.archived;
+                })
                 .reduce(function(debts, current) {
                     var result = current.result
                         .filter(function(x) {
                             return !x.settled;
+                        })
+                        .map(function(x) {
+                            x.settlementId = current[persistenceService.KEY_NAME];
+                            return x;
                         });
                     return debts.concat(result);
                 }, []);
+        }
+
+        function toggleDebt(debt) {
+            var settlement = data.filter(function(x) {
+                return x[persistenceService.KEY_NAME] === debt.settlementId;
+            }).shift();
+
+            debt.settled = !debt.settled;
+
+            settlement.result = settlement.result
+                .map(function(x) {
+                    if (x.transactionId === debt.transactionId) {
+                        x.settled = debt.settled;
+                    }
+                    return x;
+                });
+
+            persistenceService.update(
+                settlement[persistenceService.KEY_NAME],
+                settlement
+            );
         }
     }
 })();
