e27c4d5ada7596e4e8d8c4a4969f5ee62a6e7926
Filimon Danopoulos <filimon.danopoulos@gmail.com>
2015-05-25 21:58:38 +0200

    Adde manual handling of splash screen

--- www/app/app.js
+++ www/app/app.js
@@ -3,6 +3,7 @@
 	var dependencies = [
 		/* Framework specific includes */
 		'ionic',
+		'ngCordova',
 		/* Application modules */
 		'settler.common',
 		'settler.menu',
@@ -17,16 +18,14 @@
 		.run(run)
 		.config(config);
 
-	run.$inject = ['$ionicPlatform'];
-	function run($ionicPlatform) {
+	run.$inject = ['$ionicPlatform', '$cordovaSplashscreen'];
+	function run($ionicPlatform, $cordovaSplashscreen) {
 		$ionicPlatform.ready(function() {
-			// Hide the accessory bar by default (remove this to show the accessory
-			// bar above the keyboard for form inputs)
+			$cordovaSplashScreen.hide();
 			if (window.cordova && window.cordova.plugins.Keyboard) {
 				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 			}
 			if (window.StatusBar) {
-				// org.apache.cordova.statusbar required
 				StatusBar.styleDefault();
 			}
 		});
